b535ff490999d203fb73a4774618d1f2413941e7
Saumya Sharma <saumyasharma@Saumyas-MacBook-Pro-3.local>
2022-12-05 18:39:59 -0500

    added cta section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="jsm__cta section__margin section__padding">
+      <div className="jsm__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="jsm__cta-button">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;

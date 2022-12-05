c573f55de34365d69d1d5e2203c2df343e2e8e61
Saumya Sharma <saumyasharma@Saumyas-MacBook-Pro-3.local>
2022-12-05 16:18:49 -0500

    added features section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({ title, text }) => {
+  return (
+    <div className="jsm__features-container__feature">
+      <div className="jsm__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="jsm__features-container__feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;

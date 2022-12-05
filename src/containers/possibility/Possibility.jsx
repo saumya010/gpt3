0c5c149e51ac670f7fd95f0da8140a7f4bf1cf71
Saumya Sharma <saumyasharma@Saumyas-MacBook-Pro-3.local>
2022-12-05 18:28:03 -0500

    added possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./possibility.css";
+import possibility from "../../assets/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="jsm__possibility section__padding" id="possibility">
+      <div className="jsm__possibility-image">
+        <img src={possibility} alt="Possibility" />
+      </div>
+      <div className="jsm__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h2 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h2>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;

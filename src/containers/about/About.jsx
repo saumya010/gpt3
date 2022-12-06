383c62b5b05c5f8dbcd6142323f27a34ea217df6
Saumya Sharma <saumyasharma@Saumyas-MacBook-Pro-3.local>
2022-12-06 11:55:18 -0500

    fixed styling

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -5,7 +5,7 @@ import "./about.css";
 const About = () => {
   return (
     <div className="jsm__about section__margin" id="about">
-      <div className="jsm__about-feature">
+      <div className="jsm__about-feature jsm__about-container__feature-one">
         <Feature
           title="What is GPT-3"
           text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
